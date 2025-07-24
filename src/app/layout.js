e441bd8d8f244a2127d91da4898ec57313f43c5e
SooY2 <ssy67366736@gmail.com>
2025-07-24 11:00:04 +0900

    feat: app-page setting

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,11 +1,17 @@
+import Header from "@/components/common/Header";
 import "./globals.css";
+import Footer from "@/components/common/Footer";
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="ko">
       <title>우리로그</title> 
       <body>
-        {children}
+        <Header/>
+        <main>
+          {children}
+        </main>
+        <Footer/>
       </body>
     </html>
   );
