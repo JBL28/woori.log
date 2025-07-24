32ed2539b4877a1211e5391849e23b18ce8ad5db
JBLee <rabid28@naver.com>
2025-07-24 15:27:14 +0900

    feat: 마크다운 문서 작성, 조회 기능 실험추가

--- src/app/MDTest/page.js
+++ src/app/MDTest/page.js
@@ -1,18 +1,26 @@
-import Image from "next/image";
+"use client"
+
+// MD 에디터에서 값 받아오는 방법.
+// 1. useRef 임포트
+// 2. useRef를 사용할 변수 선언
+// 3. MDEdit 컴포넌트의 prop으로 getValue 넘겨주기
+
 import MDEdit from "@/components/posts/MDEdit";
+// 1단계. useRef 임포트
+import { useRef } from "react";
 
 export default function Home() {
+  // 2단계. useRef를 사용할 변수 선언
+  const getValue = useRef("");
+
   return (
     <div className="flex flex-col h-screen">
-      {/* 고정 헤더 */}
-      <header className="h-16 bg-blue-600 text-white flex items-center justify-center shadow-md z-10">
-        <h1 className="text-xl font-bold">더미 헤더</h1>
-      </header>
-
-      {/* 스크롤 가능한 콘텐츠 영역 */}
       <main className="flex-1 overflow-y-auto bg-gray-100 p-4">
-        <MDEdit height="500"/>
+        {/* 3단계. MDEdit 컴포넌트의 prop으로 getValue 넘겨주기 */}
+        {/* height prop으로 초기 높이 설정 가능합니다. (사용자가 늘렸다 줄였다 가능) */}
+        <MDEdit height="500" getValue={getValue}/>
       </main>
+      <button onClick={() =>console.log(getValue.current)}>클릭</button>
     </div>
   );
 }
